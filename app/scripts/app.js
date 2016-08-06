48292803c30d511fc7170c79b7b0a1c4db8ab91a
Mia Wang <mia.d.wang@gmail.com>
2016-08-07 00:42:46 +0800

    Finish Udacity quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,9 +64,13 @@ Hint: you'll probably still need to use .map.
 
       addSearchHeader(response.query);
 
-      response.results.map(function(url) {
-        getJSON(url).then(createPlanetThumb);
-      });
+      Promise.all(response.results.map(getJSON))
+        .then(function(data) {
+          data.forEach(createPlanetThumb);
+        })
+        .catch(function(e) {
+          console.log(e);
+        });
     });
   });
 })(document);
