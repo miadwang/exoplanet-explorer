e47608a19759ce8cabd3a61bc942372b85d9fb9f
Mia Wang <mia.d.wang@gmail.com>
2016-08-07 12:48:52 +0800

    Finish Udacity quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,11 +26,24 @@ proper order even if all the requests haven't finished.
    * @param  {Object} data - The raw data describing the planet.
    */
   function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
+    return new Promise(function(resolve) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+      console.log('rendered: ' + data.pl_name);
+      resolve();
+    });
+
+    // var pT = document.createElement('planet-thumb');
+    // for (var d in data) {
+    //   pT[d] = data[d];
+    // }
+    // home.appendChild(pT);
+    // console.log('rendered: ' + data.pl_name);
+    // return Promise.resolve();
+    // It's the same as above but a second solution
   }
 
   /**
@@ -48,8 +61,19 @@ proper order even if all the requests haven't finished.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
+    console.log('sent: ' + url);
     return get(url).then(function(response) {
-      return response.json();
+      if (url === 'data/planets/Kepler-62f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url);
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url);
+        return response.json();
+      }
     });
   }
 
@@ -58,6 +82,18 @@ proper order even if all the requests haven't finished.
     /*
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json').then(function(response) {
+
+      var sequence = response.results.map(function(url) {
+        return getJSON(url);
+      });
+      var planets = Promise.resolve();
+      sequence.forEach(function(promise) {
+        planets = planets.then(function() {
+          return promise.then(createPlanetThumb);
+        });
+      });
+
+    });
   });
 })(document);
